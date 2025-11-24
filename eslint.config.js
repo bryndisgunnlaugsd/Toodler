1fe3927bd669b8ea8ac41bf12c972dd8528b5cb5
arnarmani11 <arnarmani03@gmail.com>
2025-11-24 12:56:48 +0000

    more lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,52 @@ const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
   expoConfig,
+{
+  rules: {
+    // --- Naming ---
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'function',
+        format: ['snake_case'],
+      },
+      {
+        selector: 'class',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['PascalCase'],
+      },
+    ],
+
+    // --- Docstrings ---
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+        },
+      },
+    ],
+    'valid-jsdoc': 'error',
+
+    // --- Spacing ---
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'function', next: 'function' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+    'no-trailing-spaces': 'error',
+
+    // --- Basic formatting ---
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'eqeqeq': ['error', 'always'],
+    'max-len': ['error', { code: 100 }],
+  },
+},
   {
     ignores: ['dist/*'],
   },
