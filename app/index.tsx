16eb2302d41791073bcd5e4b42f05ba0c267d50b
bryndisgunnlaugsd <bryndisg24@ru.is>
2025-11-25 09:36:35 +0000

    vesen a index.tsx þurfti að commita aftur

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,6 @@
-<<<<<<< HEAD
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
-=======
 import { Main } from '@/src/views/main/main';
 
 
 export default Main;
   
 
->>>>>>> origin/Dev
