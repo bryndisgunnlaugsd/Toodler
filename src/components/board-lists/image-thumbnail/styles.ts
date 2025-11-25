15fb9b1377a88de34d30de3557587b1889fa165e
bryndisgunnlaugsd <bryndisg24@ru.is>
2025-11-25 16:30:41 +0000

    shadow effect a board style

--- src/components/board-lists/image-thumbnail/styles.ts
+++ src/components/board-lists/image-thumbnail/styles.ts
@@ -1,36 +1,48 @@
 import { StyleSheet } from "react-native";
+import { white } from '@/src/styles/colors';
 
 export default StyleSheet.create({
+    shadowWrapper: {
+        marginVertical: 10,
+        marginHorizontal: 16,
+        width: 350,
+        borderRadius: 16,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 6,
+        elevation: 8,
+    },
+
     card: {
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    overflow: "hidden",
-    marginVertical: 10,
-    width: 300,                 
-    shadowColor: "#000",
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    elevation: 5,               
-},
+        backgroundColor: white,
+        borderRadius: 16,
+        overflow: "hidden",
+    },
 
-image: {
-    width: "100%",
-    height: 180,
-},
+    image: {
+        width: "100%",
+        height: 180,
+    },
 
-textContainer: {
-    padding: 10,
-},
+    textContainer: {
+        padding: 10,
+        backgroundColor: white,
+    },
 
-name: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 4,
-},
+    name: {
+        fontSize: 18,
+        fontWeight: "bold",
+        marginBottom: 4,
+    },
 
-description: {
-    fontSize: 14,
-    color: "#555",
-},
+    description: {
+        fontSize: 14,
+        color: "#555",
+        lineHeight: 20,
+    },
 
 })
--- src/components/board-lists/styles.ts
+++ src/components/board-lists/styles.ts
@@ -1,29 +1,25 @@
 import { StyleSheet } from "react-native";
+import { white } from '@/src/styles/colors';
 
 export default StyleSheet.create({
     listContainer: {
         flex: 1,
-    },
-
-    container: {
-        flex: 1,
-        padding:20,
+        backgroundColor: white,
     },
 
     plusbutton: {
-    backgroundColor: "#fcb103",
-    width: 50,
-    height: 50,
-    borderRadius: 30,
-    position: "absolute",
-    bottom: 20,
-    right: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth:0.75,
-    borderColor: "Black",
+        backgroundColor: "#fcb103",
+        width: 50,
+        height: 50,
+        borderRadius: 30,
+        position: "absolute",
+        bottom: 20,
+        right: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth:0.75,
+        borderColor: "Black",
     },
-
     
     plusbuttontext: {
         color: "white",
