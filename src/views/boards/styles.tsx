42355bab209106437012c672791f7d116191e3bc
dagursmari <dagurss24@ru.is>
2025-11-25 16:37:43 +0000

    small change to board style

--- src/views/boards/styles.tsx
+++ src/views/boards/styles.tsx
@@ -4,10 +4,11 @@ import { primaryBlue, gray700, primaryRed, white } from '@/src/styles/colors';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
+        padding: 15,
         backgroundColor: white,
         alignItems: "center",
-        justifyContent: "flex-start"
+        justifyContent: "flex-start",
+        overflow: "hidden"
     },
 
     button: {
