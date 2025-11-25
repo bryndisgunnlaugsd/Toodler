97c5e1cefd3a50f31f3705bdc8dc18bd1de21e17
dagursmari <dagurss24@ru.is>
2025-11-25 22:19:13 +0000

    linting fixes

--- src/components/list-catalogue/styles.ts
+++ src/components/list-catalogue/styles.ts
@@ -1,99 +1,99 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 16,
-    paddingTop: 20,
-  },
+    container: {
+        flex: 1,
+        paddingHorizontal: 16,
+        paddingTop: 20,
+    },
 
-  /* ------------ LIST CARD ------------ */
+    /* ------------ LIST CARD ------------ */
 
-  listItem: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    listItem: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
 
-    borderRadius: 18,
+        borderRadius: 18,
 
-    paddingHorizontal: 16,
-    paddingVertical: 18,
-    marginBottom: 20,
+        paddingHorizontal: 16,
+        paddingVertical: 18,
+        marginBottom: 20,
 
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowOffset: { width: 0, height: 3 },
-    shadowRadius: 6,
-    elevation: 4,
+        shadowColor: "#000",
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 3 },
+        shadowRadius: 6,
+        elevation: 4,
 
-    position: "relative", // allow absolute children
-    zIndex: 1,
-    overflow: "visible",  // make sure menu can extend outside card
-  },
+        position: "relative", // allow absolute children
+        zIndex: 1,
+        overflow: "visible",  // make sure menu can extend outside card
+    },
 
-  // extra style applied when menu is open
-  listItemActive: {
-    zIndex: 100,
-    elevation: 20,
-  },
+    // extra style applied when menu is open
+    listItemActive: {
+        zIndex: 100,
+        elevation: 20,
+    },
 
-  listInfo: {
-    flex: 1,
-  },
+    listInfo: {
+        flex: 1,
+    },
 
-  listName: {
-    fontSize: 18,
-    fontWeight: "600",
-    color: "#000",
-  },
+    listName: {
+        fontSize: 18,
+        fontWeight: "600",
+        color: "#000",
+    },
 
-  /* ------------ THREE DOTS MENU ------------ */
+    /* ------------ THREE DOTS MENU ------------ */
 
-  listMenuWrapper: {
-    position: "relative",
-    paddingLeft: 10,
-  },
+    listMenuWrapper: {
+        position: "relative",
+        paddingLeft: 10,
+    },
 
-  listMenuButton: {
-    padding: 6,
-  },
+    listMenuButton: {
+        padding: 6,
+    },
 
-  listMenuIcon: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#000",
-  },
+    listMenuIcon: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#000",
+    },
 
-  listMenu: {
-    position: "absolute",
-    top: 28,
-    right: 0,
+    listMenu: {
+        position: "absolute",
+        top: 28,
+        right: 0,
 
-    backgroundColor: "#fff",
-    borderRadius: 12,
-    paddingVertical: 10,
-    paddingHorizontal: 16,
-    width: 150,
+        backgroundColor: "#fff",
+        borderRadius: 12,
+        paddingVertical: 10,
+        paddingHorizontal: 16,
+        width: 150,
 
-    shadowColor: "#000",
-    shadowOpacity: 0.2,
-    shadowOffset: { width: 0, height: 4 },
-    shadowRadius: 8,
-    elevation: 21,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 0, height: 4 },
+        shadowRadius: 8,
+        elevation: 21,
 
-    zIndex: 101,
-  },
+        zIndex: 101,
+    },
 
-  listMenuItem: {
-    fontSize: 15,
-    paddingVertical: 6,
-    color: "#000",
-  },
+    listMenuItem: {
+        fontSize: 15,
+        paddingVertical: 6,
+        color: "#000",
+    },
 
-  listMenuItemDelete: {
-    fontSize: 15,
-    paddingVertical: 6,
-    color: "#d11",
-    fontWeight: "600",
-  },
+    listMenuItemDelete: {
+        fontSize: 15,
+        paddingVertical: 6,
+        color: "#d11",
+        fontWeight: "600",
+    },
 });
