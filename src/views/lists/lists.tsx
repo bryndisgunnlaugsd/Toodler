20611ea247f5151818b9a5f8bdd0008eef8a6989
dagursmari <dagurss24@ru.is>
2025-11-25 10:45:47 +0000

    adding the name of list to top of page and starting file structure for create list

--- src/views/lists/lists.tsx
+++ src/views/lists/lists.tsx
@@ -3,23 +3,26 @@ import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import styles from "./styles";
 import data from "../../data/data.json";
 import { ListCatalogue } from "@/src/components/list-catalogue/list-catalogue";
+import { Board } from "@/src/types/board"
 
 export function Lists() {
     const router = useRouter();
     const { boardId } = useLocalSearchParams();
 
-    const filteredLists = data.lists.filter(
-        list => list.boardId === Number(boardId)
-    );
+    const currentBoard: Board | undefined = data.boards.find(
+    (board) => board.id === Number(boardId)
+  );
 
     return(
         
         <View style={styles.container}>
             <Text style={styles.title}>
-                Lists for Board {boardId}
+                {currentBoard?.name}
             </Text>
             
             <ListCatalogue/>
+
+            
         </View>
     )
 }
