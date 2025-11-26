6baa74bdd856962eaf69d20b985c425ec42b5718
dagursmari <dagurss24@ru.is>
2025-11-26 12:50:37 +0000

    small fix

--- src/views/lists/lists.tsx
+++ src/views/lists/lists.tsx
@@ -32,8 +32,9 @@ export function Lists() {
                 <Text style={styles.addButton}>＋</Text>
             </TouchableOpacity>
 
-        <ListCatalogue/>
+            <ListCatalogue/>
 
+            </View>
         </View>
     )
 }
